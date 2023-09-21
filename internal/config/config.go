665208f12e04ef9f8bb3c45c38606b405e62b710
Antonio Pagano <apagano@wawand.co>
2023-09-21 18:54:52 -0500

    task: adding sample .env and removing envor files

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,13 @@ import (
 )
 
 var (
-	// DatabaseURL to connect and interact with our database instance.
-	DatabaseURL = envor.Get("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/app?sslmode=disable")
-
 	// Environment in which the application is running, this is useful
 	// to determine the way we'll run the application, for example, if
 	// we're running in production we might want to disable debug mode.
 	Environment = envor.Get("GO_ENV", "development")
+
+	// DatabaseURL to connect and interact with our database instance.
+	DatabaseURL = envor.Get("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/app?sslmode=disable")
 
 	// Port in which the web application listens on.
 	Port = envor.Get("PORT", "3000")
