e515dc82dd95e60df4f0cccf0960a79e02dc6647
Antonio Pagano <apagano@wawand.co>
2023-09-21 22:13:38 -0500

    task: a bit more tweaking

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -8,9 +8,23 @@ import (
 )
 
 func main() {
-	tailo.Build()
+	tailo.Build(
+		tailo.UseInputPath("internal/web/assets/application.css"),
+		tailo.UseConfigPath("internal/config/tailwind.config.js"),
+		tailo.UseOutputPath("internal/web/public/application.css"),
+	)
 
-	cmd := exec.Command("go", "build", `--ldflags`, `-linkmode=external -extldflags="-static"`, `-tags`, `osusergo,netgo,musl`, `-buildvcs=false`, "-o", "bin/app", "cmd/app/main.go")
+	cmd := exec.Command("go", "build")
+	cmd.Args = append(
+		cmd.Args,
+
+		`--ldflags`, `-linkmode=external -extldflags="-static"`,
+		`-tags`, `osusergo,netgo,musl`,
+		`-buildvcs=false`,
+		"-o", "bin/app",
+		"cmd/app/main.go",
+	)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
